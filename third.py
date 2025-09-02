05ca78c767f38ca13663bda48ca64530c7d5f7fa
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2025-09-02 10:12:57 +0300

    banks deposit

--- third.py
+++ third.py
@@ -29,16 +29,66 @@
 
 
 
+# THIS IS EXAMPLE OF BOOK CLASS AND TO PRINT AUTHOR NAME AND BOOK TITLE 
+# class Book:
+#     def __init__(self,title,author):
+#         self.title=title
+#         self.author=author
 
-class Book:
-    def __init__(self,title,author):
-        self.title=title
-        self.author=author
-
-    def label(self):
-        return f"{self.title}-{self.author}"
+#     def label(self):
+#         return f"{self.title}-{self.author}"
     
 
-b=Book("clean code","Ghan")    
+# b=Book("clean code","Ghan")    
 
-print(b.label())
+# print(b.label())
+
+
+
+
+
+class Wallet:
+    def __init__(self,balance:float=0):
+        self.__balance=balance
+
+
+
+    def deposit(self,amount:float):
+        if amount<=0:
+            raise ValueError("Deposit amount must be positive")
+        
+        self.__balance+=amount
+
+
+    def withdraw(self,amount:float):
+        if amount<=0:
+            raise ValueError("withdraw amoutn must be postitive")
+        
+        self.__balance-=amount
+
+
+    def get_balance(self)->float:
+        return self.__balance
+    
+
+
+
+w=Wallet(1000)
+
+w.deposit(500)
+w.withdraw(200)
+
+
+print(w.get_balance())
+
+
+
+
+
+
+
+
+
+
+
+
