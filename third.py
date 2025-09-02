59e9baa671e6833818b8a44ee338e1c4d0bf6a2e
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2025-09-02 09:57:59 +0300

    Book and label

--- third.py
+++ third.py
@@ -1,28 +1,44 @@
-class BankAccount:
-    def __init__(self,owner,balance):
-        self.owner=owner
-        self.__balance=0
+# class BankAccount:
+#     def __init__(self,owner,balance):
+#         self.owner=owner
+#         self.__balance=0
 
 
-    def get_balance(self):
-        return self.__balance
+#     def get_balance(self):
+#         return self.__balance
     
-    def set_balance(self,new_balance):
-        self.__balance=new_balance
+#     def set_balance(self,new_balance):
+#         self.__balance=new_balance
 
-    def withdraw(self,amount):
-        self.__balance-=amount
-
-
-    def deposit(self,amount):
-        self.__balance+=amount
+#     def withdraw(self,amount):
+#         self.__balance-=amount
 
 
+#     def deposit(self,amount):
+#         self.__balance+=amount
 
 
-if __name__=="__main__":
-    account=BankAccount("Alice",1000)
-    account.deposit(500)
-    account.withdraw(200)
-    print(account.owner)
-    print(account.get_balance())
+
+
+# if __name__=="__main__":
+#     account=BankAccount("Alice",1000)
+#     account.deposit(500)
+#     account.withdraw(200)
+#     print(account.owner)
+#     print(account.get_balance())
+
+
+
+
+class Book:
+    def __init__(self,title,author):
+        self.title=title
+        self.author=author
+
+    def label(self):
+        return f"{self.title}-{self.author}"
+    
+
+b=Book("clean code","Ghan")    
+
+print(b.label())
